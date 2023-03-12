e00beb167e8965ec336f7b4ccac45062913e4289
Reema CK <Reema ck>
2023-03-12 11:56:06 +0530

    added the icons

--- app/model/model.component.ts
+++ app/model/model.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { faSteam } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-model',
   templateUrl: './model.component.html',
@@ -11,6 +12,7 @@ export class ModelComponent {
   myLinkedinIcon=faLinkedin;
   myTrashIcon=faTrash;
   myPenToSquareIcon=faPenToSquare;
+  myfaSteamIcon=faSteam;
   list:any[]=['item1','item1','item1'];
    listItems:any[]=[{model:"Model1",cell:"Physics"},{model:"Model2",cell:"Physics"},{model:"Model3",cell:"Physics"},{model:"Model4",cell:"Physics"},{model:"Model5",cell:"Physics"}];
    
