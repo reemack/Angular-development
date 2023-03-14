97c44ae3ae2141bf9be5e321ff02c47f2fe481ab
Reema CK <Reema ck>
2023-03-14 11:24:19 +0530

    added

---
+++ src/app/model/model.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { faSteam } from '@fortawesome/free-brands-svg-icons';
+@Component({
+  selector: 'app-model',
+  templateUrl: './model.component.html',
+  styleUrls: ['./model.component.css']
+})
+export class ModelComponent {
+  myLinkedinIcon=faLinkedin;
+  myTrashIcon=faTrash;
+  myPenToSquareIcon=faPenToSquare;
+  myfaSteamIcon=faSteam;
+   listItems:any[]=[{model:"Model1",cell:"Physics"},{model:"Model2",cell:"Physics"},{model:"Model3",cell:"Physics"},{model:"Model4",cell:"Physics"},{model:"Model5",cell:"Physics"}];
+   
+}
